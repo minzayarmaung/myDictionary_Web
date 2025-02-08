423c2643cb96e7ba01765ea50445fffa11caa021
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-02-08 22:48:30 +0630

    adding

--- myDictionary/src/main/java/com/myProject/myDictionary/Controller/dictionaryApi.java
+++ myDictionary/src/main/java/com/myProject/myDictionary/Controller/dictionaryApi.java
@@ -1,7 +1,9 @@
 package com.myProject.myDictionary.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 public class dictionaryApi {
 
+    @PostMapping("/")
+    public ResponseEntity<dictionaryApi>(){
 
+
+        return ResponseEntity.ok();
+    }
 }
